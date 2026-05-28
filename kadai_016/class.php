b245f9f558b14d5085d7c961a7034149de8173cb
khayashiharu <khayashiharu@gmail.com>
2026-05-28 21:03:04 +0900

    課題16提出

---
+++ kadai_016/class.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // クラスを定義する
+      class Food{
+        public $name;
+        public $price;
+
+        // メソッドを定義する
+        public function show_price(string $price) {
+          $this->price = $price;
+          echo $this->price.'<br>';
+        }
+    }
+
+      class Animal{
+        public $name;
+        public $height;
+        public $weight;
+
+        // メソッドを定義する
+        public function show_height(string $height) {
+          $this->height = $height;
+          echo $this->height. '<br>';
+        }
+      }
+
+       // インスタンス化する
+       $vege = new Food();
+       $vege->name='potato';
+       $vege->price='250';
+       //echo $vege->name;
+       print_r($vege);
+       echo '<br>';
+
+
+       // インスタンス化する
+       $dog = new Animal();
+       $dog->name='dog';
+       $dog->height='60';
+       $dog->weight='5000';
+       //echo $dog->name;
+       print_r($dog);
+       echo '<br>';
+
+
+       // メソッドにアクセスして実行する
+       $vege2 = new Food();
+       $vege2->show_price('250');
+
+       // メソッドにアクセスして実行する
+       $dog2 = new Animal();
+       $dog2->show_height('5000');
+
+       ?>
+   </p>
+</body>
+
+</html>
