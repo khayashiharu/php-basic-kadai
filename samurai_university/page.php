6b718b0dea63239a24f4220ec4645630142cb06f
khayashiharu <khayashiharu@gmail.com>
2026-06-17 12:59:47 +0900

    修正

--- samurai_university/page.php
+++ samurai_university/page.php
@@ -32,7 +32,7 @@
                 </div>
               </div>
             </div>
-        
+         
         <!-- Course Sidebar -->
         <div class="col-lg-4" style="background-color: #2b7b8e33">
         <?php get_sidebar();?> 
